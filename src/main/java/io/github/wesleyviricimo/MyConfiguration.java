2c34a70da2da21d0bd1aec90161ca4e795907d5f
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-05-16 21:54:57 -0300

    Trabalhando com perfis e ambientes de desenvolvimento e produção

--- src/main/java/io/github/wesleyviricimo/MyConfiguration.java
+++ src/main/java/io/github/wesleyviricimo/MyConfiguration.java
@@ -1,13 +1,18 @@
 package io.github.wesleyviricimo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration //Definindo que a classe será apenas de configuração
+@Profile("production")
 public class MyConfiguration {
 
-    @Bean(name = "applicationName") //Annotation bean cria o objeto no contexto da aplicação para ser utilizado onde for necessário
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar(){//Quando a aplicação é iniciada todos os beans com o método commandoLineRunner  são executados primeiro
+        return args -> {
+            System.out.println("RODANDO EM DESENVOLVIMENTO");
+        };
     }
 }
